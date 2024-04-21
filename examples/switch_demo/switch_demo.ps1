b0cf0128daf71aad33691fbb29ac5bee29b55f75
Joshua Haupt <joshua@hauptj.com>
2024-04-21 16:14:00 -0500

    create and delete specified files in specified dir1

--- examples/switch_demo/switch_demo.ps1
+++ examples/switch_demo/switch_demo.ps1
@@ -26,6 +26,31 @@ function Manage-Files {
         [string] $directoryPath,
         [string[]] $filesToManage
     )
+    
+    try {
+        foreach($file in $filesToManage) {
+            $filePath = Join-Path $directoryPath $file
+            if(!$deleteFiles) {
+                if(-Not (Test-Path -Path $filePath)){
+                    New-Item -Path $filePath -type file -Force
+                    Write-Host "Created file: $filePath"
+                } else {
+                    Write-Host "File $filePath already existed"
+                }
+            } elseif($deleteFiles) {
+                if(Test-Path -Path $filePath){
+                    Remove-Item -Path $filePath
+                    Write-Host "Deleted file: $filePath"
+                } else {
+                    Write-Host "File $filePath does not exist"
+                }
+            } else {
+                Write-Host "Invalid parameter passed in for deleteFiles: $deleteFiles"
+            }
+        }
+    } catch {
+        Write-Host "An unresolved exception occured"
+    }
 }
 
 function Word-Hash-Map {
